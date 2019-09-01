44881021815af9d87c826327da74b44a94801c63
Jimmy <iamjimmyjimmy@yahoo.com>
2019-09-01 16:58:10 -0700

    Added: Heart Zone BLE Filtering | Edit: bluetooth package / Fixed Style
    Added functionality to look for only Heart Zone heart rate monitors. Updated and Migrated project to Nativescript 6.0.3 (current version as of right now). Updated code to work with new Bluetooth package update. Began to show RSSI signal on UI.

--- app/page_connection/connection-view-model.js
+++ app/page_connection/connection-view-model.js
@@ -2,143 +2,163 @@ var observable = require("tns-core-modules/data/observable");
 var observableArray = require("tns-core-modules/data/observable-array");
 var frameModule = require("tns-core-modules/ui/frame");
 var dialogs = require("tns-core-modules/ui/dialogs");
-var bluetooth = require("nativescript-bluetooth");
+var {
+    Bluetooth
+} = require('nativescript-bluetooth');
+const bluetooth = new Bluetooth();
 
-var ConnectionViewModel = (function (_super){
-  __extends(ConnectionViewModel, _super);
-  function ConnectionViewModel() {
-    _super.call(this);
-  }
-  // Bluetooth Peripherals
-  var observablePeripheralArray = new observableArray.ObservableArray();
-  ConnectionViewModel.prototype.peripherals = observablePeripheralArray;
+var ConnectionViewModel = (function (_super) {
+    __extends(ConnectionViewModel, _super);
 
-
-  ConnectionViewModel.prototype.checkBluetoothEnabled = function(){
-    bluetooth.isBluetoothEnabled().then(function(enabled) {
-      dialogs.alert({
-        title: "Enabled?",
-        message: enabled ? "Yes" : "No",
-        okButtonText: "OK, thanks"
-      });
-    });
-  }
-  ConnectionViewModel.prototype.enableBluetooth = function () {
-    bluetooth.enable().then(function(enabled) {
-      setTimeout(function() {
-        dialogs.alert({
-          message: "Bluetooth was " + (enabled ? "" : "not") + " enabled on this device.",
-          okButtonText: "Continue"
-        });
-      }, 500);
-    });
-  };
-
-  ConnectionViewModel.prototype.onPeripheralTap = function (args) {
-    var index = args.index;
-    console.log('!!&&&&***** Clicked item with index ' + index);
-    var peri = DemoAppModel.prototype.peripherals.getItem(index);
-    console.log("--- peri selected: " + peri.UUID);
-
-    var navigationEntry = {
-      moduleName: "services-page",
-      context: {
-        info: "something you want to pass to your page",
-        foo: 'bar',
-        peripheral: peri
-      },
-      animated: true
-    };
-    var topmost = frameModule.topmost();
-    topmost.navigate(navigationEntry);
-  };
-
-
-  ConnectionViewModel.prototype.doStopScanning = function () {
-    var that = this;
-    bluetooth.stopScanning().then(function() {
-      that.set('isLoading', false);
-    },
-    function (err) {
-      dialogs.alert({
-        title: "Whoops!",
-        message: err,
-        okButtonText: "OK, so be it"
-      });
-    });
-  };
-  ConnectionViewModel.prototype.doStartScanning = function () {
-    var that = this;
-
-    that.set('isLoading', true);
-    // reset the array
-    observablePeripheralArray.splice(0, observablePeripheralArray.length);
-    bluetooth.startScanning(
-      {
-        serviceUUIDs: [], // pass an empty array to scan for all services
-        seconds: 4, // passing in seconds makes the plugin stop scanning after <seconds> seconds
-        onDiscovered: function (peripheral) {
-          observablePeripheralArray.push(observable.fromObject(peripheral));
-        }
-      }
-    ).then(function() {
-      that.set('isLoading', false);
-    },
-    function (err) {
-      that.set('isLoading', false);
-      dialogs.alert({
-        title: "Whoops!",
-        message: err,
-        okButtonText: "OK, got it"
-      });
-    });
-  };
-
-  ConnectionViewModel.prototype.doScanForHeartrateMontitor = function () {
-    var that = this;
-    var omegaService = "180D";
-    that.set('isLoading', true);
-    // reset the array
-    observablePeripheralArray.splice(0, observablePeripheralArray.length);
-    bluetooth.startScanning(
-      {
-        serviceUUIDs: [omegaService], // pass an empty array to scan for all services
-        seconds: 4, // passing in seconds makes the plugin stop scanning after <seconds> seconds
-        onDiscovered: function (peripheral) {
-          observablePeripheralArray.push(observable.fromObject(peripheral));
-        }
-      }
-    ).then(function() {
-      that.set('isLoading', false);
-    },
-    function (err) {
-      that.set('isLoading', false);
-      dialogs.alert({
-        title: "Whoops!",
-        message: err,
-        okButtonText: "OK, got it"
-      });
-    });
-  };
-  ConnectionViewModel.prototype.onPeripheralTap = function (args) {
-    console.log("HEREISON")
-    var index = args.index;
-    console.log('!!&&&&***** Clicked item with index ' + index);
-    var peri = ConnectionViewModel.prototype.peripherals.getItem(index);
-    console.log("--- peri selected: " + peri.UUID);
-    const button = args.object;
-    const page = button.page;
-    var navigationEntry = {
-      moduleName: "page_heartrate/heartrate-page",
-      context: {
-        peripheral: peri
-      }
+    function ConnectionViewModel() {
+        _super.call(this);
     }
-    page.frame.navigate(navigationEntry);
-  };
-  return ConnectionViewModel;
+    // Creates a Bluetooth Peripherals Observable Array Object 
+    var observablePeripheralArray = new observableArray.ObservableArray();
+    // Have the viewmodel listen onto obesrvable
+    ConnectionViewModel.prototype.peripherals = observablePeripheralArray;
+    /**
+     * Checks if the device's bluetooth is enabled
+     */
+    ConnectionViewModel.prototype.checkBluetoothEnabled = function () {
+        bluetooth.isBluetoothEnabled().then(function (enabled) {
+            dialogs.alert({
+                title: "Enabled?",
+                message: enabled ? "Yes" : "No",
+                okButtonText: "OK, thanks"
+            });
+        });
+    };
+    /**
+     * Enable's device's bluetooth if it is not enabled
+     */
+    ConnectionViewModel.prototype.enableBluetooth = function () {
+        bluetooth.enable().then(function (enabled) {
+            setTimeout(function () {
+                dialogs.alert({
+                    message: "Bluetooth was " + (enabled ? "" : "not") + " enabled on this device.",
+                    okButtonText: "Continue"
+                });
+            }, 500);
+        });
+    };
+    /**
+     * Force Stop Scanning for BLE devices
+     */
+    ConnectionViewModel.prototype.doStopScanning = function () {
+        var that = this;
+        bluetooth.stopScanning().then(function () {
+                that.set('isLoading', false);
+            },
+            function (err) {
+                dialogs.alert({
+                    title: "Whoops!",
+                    message: err,
+                    okButtonText: "OK, so be it"
+                });
+            });
+    };
+    /**
+     * Start Scanning for BLE devices
+     */
+    ConnectionViewModel.prototype.doStartScanning = function () {
+        var that = this;
+        that.set('isLoading', true);
+        // This reset the current peripheral array
+        observablePeripheralArray.splice(0, observablePeripheralArray.length);
+        bluetooth.startScanning({
+            serviceUUIDs: [], // Pass an empty array to scan for all services
+            seconds: 4, // Passing in seconds makes the plugin stop scanning after <number> seconds
+            onDiscovered: function (peripheral) {
+                // Variables to check if the device was already recorded in the list
+                var duplicate = false;
+                var duplicateIndex = -1;
+                // Iterate through current list of peripherals to look for duplicates
+                for (var numberOfDevices = 0; numberOfDevices < observablePeripheralArray.length; numberOfDevices++) {
+                    if (observablePeripheralArray.getItem(numberOfDevices).UUID === peripheral.UUID) {
+                        duplicate = true;
+                        duplicateIndex = numberOfDevices;
+                        break;
+                    }
+                }
+                // If a duplicate was not found add it into the array or just update the value
+                if (!duplicate) {
+                    observablePeripheralArray.push(observable.fromObject(peripheral));
+                } else {
+                    observablePeripheralArray.setItem(duplicateIndex, observable.fromObject(peripheral))
+                }
+
+            }
+        }).then(function () {
+                that.set('isLoading', false);
+            },
+            function (err) {
+                that.set('isLoading', false);
+                dialogs.alert({
+                    title: "Whoops!",
+                    message: err,
+                    okButtonText: "OK, got it"
+                });
+            });
+    };
+    ConnectionViewModel.prototype.doScanForHeartrateMontitor = function () {
+        var that = this;
+        that.set('isLoading', true);
+        // This reset the current peripheral array
+        observablePeripheralArray.splice(0, observablePeripheralArray.length);
+        bluetooth.startScanning({
+            serviceUUIDs: ['180d'], // pass an empty array to scan for all services
+            seconds: 4, // passing in seconds makes the plugin stop scanning after <seconds> seconds
+            onDiscovered: function (peripheral) {
+               // Variables to check if the device was already recorded in the list
+               var duplicate = false;
+               var duplicateIndex = -1;
+               // Iterate through current list of peripherals to look for duplicates
+               for (var numberOfDevices = 0; numberOfDevices < observablePeripheralArray.length; numberOfDevices++) {
+                   if (observablePeripheralArray.getItem(numberOfDevices).UUID === peripheral.UUID) {
+                       duplicate = true;
+                       duplicateIndex = numberOfDevices;
+                       break;
+                   }
+               }
+               // If a duplicate was not found add it into the array or just update the value
+               // Also filters out for just Heart Zone Devices
+               if (!duplicate && (peripheral.name !==null  && peripheral.name.includes('RHYTHM'))) {
+                   observablePeripheralArray.push(observable.fromObject(peripheral));
+               } else {
+                   observablePeripheralArray.setItem(duplicateIndex, observable.fromObject(peripheral))
+               }
+            }
+        }).then(function () {
+                that.set('isLoading', false);
+            },
+            function (err) {
+                that.set('isLoading', false);
+                dialogs.alert({
+                    title: "Whoops!",
+                    message: err,
+                    okButtonText: "OK, got it"
+                });
+            });
+    };
+    ConnectionViewModel.prototype.onPeripheralTap = function (args) {
+        console.log("HEREISON")
+        var index = args.index;
+        console.log('!!&&&&***** Clicked item with index ' + index);
+        var peri = ConnectionViewModel.prototype.peripherals.getItem(index);
+        console.log("--- peri selected: " + peri.UUID);
+        const button = args.object;
+        const page = button.page;
+        var navigationEntry = {
+            moduleName: "page_heartrate/heartrate-page",
+            context: {
+                peripheral: peri
+            }
+        }
+        page.frame.navigate(navigationEntry);
+    };
+    return ConnectionViewModel;
 })(observable.Observable);
 
 
 exports.connectionViewModel = new ConnectionViewModel();
-
--- app/page_heartrate/heartrate-view-model.js
+++ app/page_heartrate/heartrate-view-model.js
@@ -2,7 +2,10 @@ var observable = require("tns-core-modules/data/observable");
 var observableArray = require("tns-core-modules/data/observable-array");
 var frameModule = require("tns-core-modules/ui/frame");
 var dialogs = require("tns-core-modules/ui/dialogs");
-var bluetooth = require("nativescript-bluetooth");
+var {
+    Bluetooth
+} = require('nativescript-bluetooth');
+const bluetooth = new Bluetooth();
 
 var HeartrateViewModel = (function (_super) {
     __extends(HeartrateViewModel, _super);
@@ -14,7 +17,7 @@ var HeartrateViewModel = (function (_super) {
         this.peripheral = args.object.navigationContext.peripheral;
         this.page = args.object;
 
-        HeartrateViewModel.prototype.heartrate.set("UUID","UUID: "+ this.peripheral.UUID)
+        HeartrateViewModel.prototype.heartrate.set("UUID", "UUID: " + this.peripheral.UUID)
         bluetooth.connect({
             UUID: this.peripheral.UUID,
             onConnected: function (peripheral) {
@@ -34,15 +37,15 @@ var HeartrateViewModel = (function (_super) {
                     characteristicUUID: '2a37',
                     onNotify: function (result) {
                         // see the read example for how to decode ArrayBuffers
-                        console.log("read: " + JSON.stringify(result));
-                        console.log(result.value)
-                        console.log(result.value.toString())
+                        // console.log("read: " + JSON.stringify(result));
+                        // console.log(result.value)
+                        // console.log(result.value.toString())
                         var view = new Int8Array(result.value);
-                        console.log(view)
-                        
-                        HeartrateViewModel.prototype.heartrate.set("HeartRate","Heart Rate: " + view[1]);
-                        HeartrateViewModel.prototype.heartrate.set("Signal","Signal: " + view[2]);
-                        HeartrateViewModel.prototype.heartrate.set("Other","Other: " + view[3]);
+                        // console.log(view)
+
+                        HeartrateViewModel.prototype.heartrate.set("HeartRate", "Heart Rate: " + view[1]);
+                        HeartrateViewModel.prototype.heartrate.set("Signal", "Signal: " + view[2]);
+                        HeartrateViewModel.prototype.heartrate.set("Other", "Other: " + view[3]);
                     }
                 }).then((result) => {
                     console.log("HEY HEY")
